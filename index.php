84a8c4c9d7b0b7b19eb2a609427a46781278ece4
Web <web@it.step>
2018-09-24 20:11:38 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+   <ul id="list">
+       <li>item1</li>
+       <li>item2</li>
+       <li>item3</li>
+       <li>item4</li>
+       <li>item5</li>
+       <li>item6</li>
+   </ul>
+   
+   <button id="btn">Click me!</button>
+   <button id="btn2">Click me!</button>
+  <div id="main" someattr="test">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, eligendi.</div>
+   
+    <script
+  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
+        <script src="scripts/script.js">
+        </script>    
+</body>
+</html>
+
